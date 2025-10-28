0002342a0a8aa3318f9b42026e8c59f774eda4e6
tr1viall <666d.aks666@gmail.com>
2025-10-28 09:02:49 +0500

    mango

--- wch/models.py
+++ wch/models.py
@@ -6,22 +6,20 @@ class Hotel(models.Model):
     phone_number = models.CharField("Номер телефона", max_length=20)
     email = models.CharField("Эл.почта", max_length=100)
     description = models.TextField("Описание отеля")
-
 class Meta:
     verbose_name_plural = "Отель"
-
 def __str__(self):
     return f"{self.name}"
+
 
 class Room_type(models.Model):
     name = models.CharField("Тип комнаты", max_length=50)
     description = models.TextField("Описание")
-
 class Meta:
     verbose_name_plural = "Тип комнаты"
-
 def __str__(self):
     return f"{self.id}"
+  
     
 class Room(models.Model):
     ST = [
@@ -30,15 +28,14 @@ class Room(models.Model):
     ]
     hotel_id = models.ForeignKey(Hotel, on_delete=models.CASCADE)
     room_type_id = models.ForeignKey(Room_type, on_delete=models.CASCADE)
-    is_available = models.CharField("Доступен или нет",)
+    is_available = models.CharField("Доступен или нет", choices=ST)
     price_per_night = models.DecimalField("Цена за ночь", max_digits=10, decimal_places=2)
-
 class Meta:
     verbose_name = "Комната"
     verbose_name_plural = "Комнаты"
-
 def __str__(self):
     return f"{self.id}"
+
 
 class Guest(models.Model):
     name = models.CharField("Имя", max_length=50)
@@ -46,13 +43,12 @@ class Guest(models.Model):
     passport_number = models.CharField("Номер паспорта", max_length=20)
     phone_number = models.CharField("Номер телефона", max_length=20)
     email = models.CharField("Эл.почта", max_length=100)
-
 class Meta:
     verbose_name = "Гость"
     verbose_name_plural = "Гости"
-
 def __str__(self):
     return f"{self.name}{self.surname}"
+
 
 class Booking(models.Model):
     ST = [
@@ -67,9 +63,7 @@ class Booking(models.Model):
     status = models.CharField("Статус бронирования", max_length=20, choices=ST)
     total_amount = models.DecimalField("Итоговая стоимость", max_digits=10, decimal_places=2)
     hotel_id = models.ForeignKey(Hotel, on_delete=models.CASCADE)
-
 class Meta:
     verbose_name_plural = "Бронирование"
-
 def __str__(self):
     return f"{self.id}"
