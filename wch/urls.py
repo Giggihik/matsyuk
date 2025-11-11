7bd5081750b81874a462746f6abfefe4e9009aaf
Sultanova Juma <giggihik@gmail.com>
2025-11-11 20:09:22 +0200

    up

--- wch/urls.py
+++ wch/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.root),
-    path('booking', views.bookings)
+    path('booking', views.bookings),
+    path('stays', views.stays),
+    path('dining', views.dinings),
+    path('aquaventure', views.aquaventure)
 ]
--- wch/views.py
+++ wch/views.py
@@ -7,3 +7,12 @@ def root(request):
 
 def bookings(request):
     return render(request, 'booking.html')
+
+def stays(request):
+    return render(request, 'stays.html')
+
+def dinings(request):
+    return render(request, 'dining.html')
+
+def aquaventure(request):
+    return render(request, 'aquaventure.html')
